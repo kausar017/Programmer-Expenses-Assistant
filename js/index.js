d10426703ece62619f8ea0e30a3642849156f843
kousra <mdkousarmia71@gmail.com>
2024-09-29 11:48:28 +0600

    problem solvig

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 
 //add Even lisatioar calculat btn
+let count = 0;
+
 const calCulatbtn = document.getElementById('calculate');
     calCulatbtn.addEventListener('click', function () {
         // console.log(calCulatbtn.innerText);
+        count += 1;
         const incomeInput = parseFloat(document.getElementById('income').value);
         const softwareInput = parseFloat(document.getElementById('software').value);
         const coursesInput = parseFloat(document.getElementById('courses').value);
@@ -21,7 +24,20 @@ const calCulatbtn = document.getElementById('calculate');
         balanceEl.innerText = balance.toFixed(2);
 
         const resultsEl = document.getElementById('results');
-        resultsEl.classList.remove('hidden')
+        resultsEl.classList.remove('hidden');
+
+        const historyItem = document.createElement("div");
+        historyItem.className= "bg-white p-3 rounded-md shadow-sm border-l-2 border-blue-500";
+        historyItem.innerHTML = `
+                <p class= "font-bold">serial: ${count}</p>
+                <p class= "text-xs text-gray-500">${new Date().toLocaleDateString()}</p>
+                <p class= "text-xs text-gray-500">Income:$${incomeInput.toFixed(2)}</p>
+                <p class= "text-xs text-gray-500">Expansess:$${totalExpansess.toFixed(2)}</p>
+                <p class= "text-xs text-gray-500">balance:$${balance.toFixed(2)}</p>
+
+        `;
+        const historyList =document.getElementById('history-list');
+            historyList.insertBefore(historyItem, historyList.firstChild)
         
     });
     
@@ -47,4 +63,34 @@ const calCulatbtn = document.getElementById('calculate');
         const remaning = balance - saving;
         remanignBalance.innerText = remaning.toFixed(2);
 
+     });
+
+
+     //assistant Tab 
+     const assistantTab = document.getElementById('assistant-tab');
+     assistantTab.addEventListener('click', function(){
+             assistantTab.classList.add( 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600', 'text-white');
+
+        const historySection = document.getElementById('history-section');
+        historySection.classList.add('hidden');
+
+        const expenseForm = document.getElementById('expense-form');
+        expenseForm.classList.remove('hidden');
+        const historyTab = document.getElementById('history-tab');
+        historyTab.classList.remove( 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600', 'text-white');
+     })
+
+     
+    //history Tab
+     const historyTab = document.getElementById('history-tab');
+     historyTab.addEventListener('click', function(){
+        historyTab.classList.add( 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600', 'text-white');
+
+        const historySection = document.getElementById('history-section');
+            historySection.classList.remove('hidden')
+
+        const expenseForm = document.getElementById('expense-form');
+            expenseForm.classList.add('hidden');
+        const assistantTab = document.getElementById('assistant-tab');
+            assistantTab.classList.remove( 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600', 'text-white');
      });
